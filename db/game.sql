d5b09de2c9556114202e8f222a37f778b4ec7f4c
KooozyMan <kooozyman@gmail.com>
2025-09-04 18:54:16 +0300

    included db

---
+++ db/game.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 04, 2025 at 05:53 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `game`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `alphabet`
+--
+
+CREATE TABLE `alphabet` (
+  `id` int(11) NOT NULL,
+  `letter` varchar(2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `alphabet`
+--
+
+INSERT INTO `alphabet` (`id`, `letter`) VALUES
+(1, 'أ'),
+(2, 'ب'),
+(3, 'ت'),
+(4, 'ث'),
+(5, 'ج'),
+(6, 'ح'),
+(7, 'خ'),
+(8, 'د'),
+(9, 'ذ'),
+(10, 'ر'),
+(11, 'ز'),
+(12, 'س'),
+(13, 'ش'),
+(14, 'ص'),
+(15, 'ض'),
+(16, 'ط'),
+(17, 'ظ'),
+(18, 'ع'),
+(19, 'غ'),
+(20, 'ف'),
+(21, 'ق'),
+(22, 'ك'),
+(23, 'ل'),
+(24, 'م'),
+(25, 'ن'),
+(26, 'هـ'),
+(27, 'و'),
+(28, 'ي');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL,
+  `type_id` int(11) DEFAULT NULL,
+  `question` varchar(1000) DEFAULT NULL,
+  `answer` varchar(255) DEFAULT NULL,
+  `letter_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `types`
+--
+
+CREATE TABLE `types` (
+  `type_id` int(11) NOT NULL,
+  `type` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `alphabet`
+--
+ALTER TABLE `alphabet`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`question_id`),
+  ADD KEY `fk_type_id` (`type_id`),
+  ADD KEY `fk_alphabet_id` (`letter_id`);
+
+--
+-- Indexes for table `types`
+--
+ALTER TABLE `types`
+  ADD PRIMARY KEY (`type_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `alphabet`
+--
+ALTER TABLE `alphabet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `types`
+--
+ALTER TABLE `types`
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `questions`
+--
+ALTER TABLE `questions`
+  ADD CONSTRAINT `fk_alphabet_id` FOREIGN KEY (`letter_id`) REFERENCES `alphabet` (`id`),
+  ADD CONSTRAINT `fk_type_id` FOREIGN KEY (`type_id`) REFERENCES `types` (`type_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
